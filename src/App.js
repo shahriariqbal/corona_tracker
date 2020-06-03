4116d0ad8c2b6d635f1ceb94df9aacb5348b45da
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-06-03 20:57:15 +0600

    get data  from selected country

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { fetchData } from './api';
 class App extends React.Component {
 	state = {
 		data: {},
+		country: '',
 	}
 
 	async componentDidMount(){
@@ -18,6 +19,18 @@ class App extends React.Component {
 
 		this.setState({ data: fetchedData})
 	}
+
+
+	handleCountryChange = async (country) => {
+		const fetchedData = await fetchData(country);
+		console.log(fetchedData);
+
+	}
+
+
+
+
+
 	render() {
 		const { data } = this.state;
 
@@ -25,7 +38,7 @@ class App extends React.Component {
 		return (
 			<div className={styles.container}>
 				<Cards data={data} />
-				<CountryPicker/>
+				<CountryPicker  handleCountryChange={this.handleCountryChange}/>
 				<Chart/>
 			</div>
 		);
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -5,7 +5,7 @@ import styles from './CountryPicker.module.css';
 
 import { fetchCountries } from '../../api';
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
 
     const [fetchedCountries, setFetchedCountries] = useState([]);
 
@@ -22,7 +22,7 @@ const CountryPicker = () => {
 
 	return (
      <FormControl className={styles.formControl}>
-         <NativeSelect>
+         <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)  }>
              <option value="global"> Global </option>
              {fetchedCountries.map((country, i)=> <option key={i} value={country} > {country} </option>)}
          </NativeSelect>
